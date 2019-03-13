1750114122912ae7e8748031a0d8ac889882403e
Matt <mcorby313@gmail.com>
2019-03-12 20:21:24 -0400

    Add full game

--- playground.py
+++ playground.py
@@ -1,91 +1,99 @@
-import pygame, sys, time
+import pygame, sys, random, os
+
+# Control where the game window appears on startup
+screen_xy = (100, 0)
+os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % screen_xy
 
 pygame.init()
-
-SCREEN = pygame.display.set_mode((800, 800))
+random.seed()
 
 # Set FPS so CPU doesn't execute blazingly fast
 FPS = 30
 fpsClock = pygame.time.Clock()
 
-GUY = pygame.image.load('dood.png')
-# Resize
-GUY = pygame.transform.scale(GUY, (100, 100))
-GUY_RECT = GUY.get_rect()
+# Initialize font
+Font = pygame.font.SysFont(None, 100)
+GameOverText = Font.render('GAME OVER', False, (255, 0, 0))
+GAME_OVER_RECT = GameOverText.get_rect()
 
-# Create square obstacle
-Square_X = 0
-Square_Y = 0
+# A Score font
+ScoreFont = pygame.font.SysFont(None, 100)
+SCORE = 0
+
+SCREEN = pygame.display.set_mode((650, 650))
+SCREEN_RECT = SCREEN.get_rect()
+
+GAME_OVER_RECT.center = SCREEN_RECT.center
+
+SPRITE = pygame.image.load('dood.png')
+
+# Resize
+SPRITE_IMAGE = pygame.transform.scale(SPRITE, (100, 120))
+SPRITE_RECT = SPRITE_IMAGE.get_rect()
+
+SPRITE_RECT.y = SCREEN_RECT.height - SPRITE_RECT.height
+SPRITE_RECT.x = SCREEN_RECT.centerx
+
 Square_Width = 100
 Square_Height = 100
+
+# Create square obstacle
+Square_X = random.randrange(0, SCREEN.get_width() - Square_Width)
+Square_Y = 0
+
+# Get rect object so we can draw it more easily
 SQUARE = pygame.Rect(Square_X, Square_Y, Square_Width, Square_Height)
 
-GUY_RECT.x = 200
-GUY_RECT.y = 200
-
-moveIncriment = 10
-
-moveDirection = "None"
-
-#for font in pygame.font.get_fonts():
-#    print(str(font))
-
-if pygame.font.get_init():
-    ComicSans = pygame.font.SysFont(None, 200)
-    GameOverText = ComicSans.render('WASTED', False, (255, 0, 0))
-    print("Font initialized")
+SQUARE_MOVEMENT = 10
+PIXELS_TO_MOVE = 10
+GAME_OVER = False
 
 while True:
     for event in pygame.event.get():
         if event.type is pygame.QUIT:
             sys.exit()
 
-        # Keep track of movement direction
-        # TODO: Place in a function
-        keysPressed = pygame.key.get_pressed()
-        if keysPressed[pygame.K_RIGHT]:
-            moveDirection = "right"
-        elif keysPressed[pygame.K_LEFT]:
-            moveDirection = "left"
-        elif keysPressed[pygame.K_UP]:
-            moveDirection = "up"
-        elif keysPressed[pygame.K_DOWN]:
-            moveDirection = "down"
+    # Sprite movement
+    keysPressed = pygame.key.get_pressed()
+    if keysPressed[pygame.K_RIGHT]:
+        SPRITE_RECT.x += PIXELS_TO_MOVE
+    elif keysPressed[pygame.K_LEFT]:
+        SPRITE_RECT.x -= PIXELS_TO_MOVE
+    elif keysPressed[pygame.K_UP]:
+        SPRITE_RECT.y -= PIXELS_TO_MOVE
+    elif keysPressed[pygame.K_DOWN]:
+        SPRITE_RECT.y += PIXELS_TO_MOVE
 
-        # If a key has been let go, stop movement
-        if event.type is pygame.KEYUP:
-            moveDirection = "None"
+    # Square movement
+    SQUARE.y += SQUARE_MOVEMENT
 
-    # Change pixel location based on movement type
-    # TODO: place in function
-    if moveDirection == "right":
-        GUY_RECT.x += 10
-    elif moveDirection == "left":
-        GUY_RECT.x -= 10
-    elif moveDirection == "up":
-        GUY_RECT.y -= 10
-    elif moveDirection == "down":
-        GUY_RECT.y += 10
+    # Regenerate another square
+    if SQUARE.y >= SCREEN.get_height():
+        SQUARE.y = 0
+        SQUARE.x = random.randrange(0, SCREEN.get_width() - Square_Width)
+        SQUARE_MOVEMENT += 1
+        SCORE += 1
 
-    # Invert the incriment value whenever the square touches the edge of the screen
-    SQUARE.x += moveIncriment
-    if SQUARE.x > (SCREEN.get_width() - SQUARE.width) or \
-    SQUARE.x <= -1:
-        moveIncriment = -moveIncriment
+    # Check for a collision
+    if SPRITE_RECT.colliderect(SQUARE):
+        # Stop square and sprite
+        SQUARE_MOVEMENT = 0
+        PIXELS_TO_MOVE = 0
+        GAME_OVER = True
 
-    #print("SQUARE: " + str(SQUARE))
-
+    # Show Square and Sprite
     SCREEN.fill((255, 255, 255))
+    SCREEN.blit(SPRITE_IMAGE, SPRITE_RECT)
     pygame.draw.rect(SCREEN, (255, 255, 0), SQUARE)
-    SCREEN.blit(GUY, (GUY_RECT.x, GUY_RECT.y))
 
-    #print("GUY RECT: " + str(GUY_RECT))
+    # Render score
+    ScoreText = Font.render("Score: " + str(SCORE), False, (0, 0, 255))
+    SCREEN.blit(ScoreText, (0, 0))
 
-    if GUY_RECT.colliderect(SQUARE):
-        SCREEN.blit(GameOverText, (200, 200))
+    # Only show game over message if the game is over... duh
+    if GAME_OVER:
+        SCREEN.blit(GameOverText, GAME_OVER_RECT)
 
     pygame.display.update()
 
-    # Keep FPS at 30 so CPU does go nuts and
-    # make the sprite shoot off in a blaze of fire
     fpsClock.tick(FPS)
